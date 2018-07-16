e4dac050f04bed3bdf868a67d9c84ef02758231a
Archie Isdiningrat <archie.isdiningrat@bizzy.co.id>
2018-07-16 10:57:44 +0700

    add fuctions

--- 00-types/app.js
+++ 00-types/app.js
@@ -4,3 +4,40 @@ var myName = 'Max';
 var age = 22;
 // boolean
 var hasHooby = true;
+// assign type
+var myRealAge;
+myRealAge = 22;
+// array
+var hobbies = ["gaming", "coding", "swimming"];
+hobbies = [100, 200, 300];
+// tuples
+var address = ["pettarani", 99];
+// enum
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 101] = "Blue";
+})(Color || (Color = {}));
+var myColor = Color.Green;
+// any
+var car = 'bmw';
+car = {
+    brand: 'bmw',
+    series: 3
+};
+// functions
+function getName(name) {
+    return name;
+}
+// void
+function sayHello() {
+    console.log('hello');
+}
+// argument types
+function multiply(val1, val2) {
+    return val1 * val2;
+}
+// function types
+var myMultiply;
+myMultiply = multiply;
--- 00-types/app.ts
+++ 00-types/app.ts
@@ -10,3 +10,45 @@ let hasHooby = true;
 // assign type
 let myRealAge: number;
 myRealAge = 22;
+
+// array
+let hobbies: any[] = ["gaming", "coding", "swimming"];
+hobbies = [100, 200, 300];
+
+// tuples
+let address: [string, number] = ["pettarani", 99];
+
+// enum
+enum Color {
+    Gray,
+    Green = 100,
+    Blue
+}
+let myColor: Color = Color.Green;
+
+// any
+let car: any = 'bmw';
+car = {
+    brand: 'bmw',
+    series: 3
+}
+
+// functions
+function getName(name): string{
+    return name;
+}
+
+// void
+function sayHello(): void{
+    console.log('hello')
+}
+
+// argument types
+function multiply(val1: number, val2: number): number{
+    return val1 * val2;
+}
+
+// function types
+let myMultiply: (val1: number, val2: number) => number;
+myMultiply = multiply;
+
