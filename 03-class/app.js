afb29b1195a5cf6442021d8ea81b8f1afaba85bf
Archie Isdiningrat <archie.isdiningrat@bizzy.co.id>
2018-07-19 12:41:09 +0700

    finish class

--- 03-class/app.js
+++ 03-class/app.js
@@ -8,6 +8,7 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+// class definition
 var Person = /** @class */ (function () {
     function Person(name, username) {
         this.username = username;
@@ -26,12 +27,91 @@ var person = new Person('archie', 'archisdiningrat');
 person.setType('cool guy');
 console.log(person);
 person.printAge();
+// class inheritance
 var Giant = /** @class */ (function (_super) {
     __extends(Giant, _super);
     function Giant(name) {
-        return _super.call(this, name, "giant-" + name) || this;
+        var _this = _super.call(this, name, "giant-" + name) || this;
+        _this.age = 99; // protected refers to super
+        return _this;
     }
     return Giant;
 }(Person));
 var giant = new Giant('angga');
 console.log(giant);
+// setter and getter
+var Plant = /** @class */ (function () {
+    function Plant() {
+        this._species = 'default';
+    }
+    Object.defineProperty(Plant.prototype, "species", {
+        get: function () {
+            return this._species;
+        },
+        set: function (value) {
+            if (value.length > 3) {
+                this._species = value;
+            }
+            else {
+                this._species = 'default';
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Plant;
+}());
+var plant = new Plant();
+console.log(plant.species);
+plant.species = 'AB';
+console.log(plant.species);
+plant.species = 'Green';
+console.log(plant.species);
+// static properties and methods
+var Helper = /** @class */ (function () {
+    function Helper() {
+    }
+    Helper.calculateCircumference = function (diameter) {
+        return this.PI * diameter;
+    };
+    Helper.PI = 3.14;
+    return Helper;
+}());
+console.log(Helper.PI);
+console.log(Helper.calculateCircumference(22));
+// abstract class
+var Project = /** @class */ (function () {
+    function Project() {
+        this.projectName = 'default';
+    }
+    Project.prototype.calcBudget = function () {
+        return this.budget * 2;
+    };
+    return Project;
+}());
+var ITProject = /** @class */ (function (_super) {
+    __extends(ITProject, _super);
+    function ITProject() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ITProject.prototype.changeName = function (name) {
+        this.projectName = name;
+    };
+    return ITProject;
+}(Project));
+var project = new ITProject();
+project.changeName('kbms');
+// private constructors and singletons
+var OnlyOne = /** @class */ (function () {
+    function OnlyOne(name) {
+        this.name = name;
+    }
+    OnlyOne.getInstance = function () {
+        if (!OnlyOne.instance) {
+            OnlyOne.instance = new OnlyOne('The Only One');
+        }
+        return OnlyOne.instance;
+    };
+    return OnlyOne;
+}());
+var only = OnlyOne.getInstance();
--- 03-class/app.ts
+++ 03-class/app.ts
@@ -28,8 +28,81 @@ person.printAge();
 class Giant extends Person {
     constructor(name: string){
         super(name, `giant-${name}`);
+        this.age = 99; // protected refers to super
     }
 }
 
 let giant = new Giant('angga');
 console.log(giant);
+
+// setter and getter
+class Plant{
+    private _species: string = 'default';
+
+    set species(value: string){
+        if (value.length > 3){
+            this._species = value;
+        } else {
+            this._species = 'default';
+        }
+    }
+
+    get species(){
+        return this._species;
+    }
+}
+
+const plant = new Plant();
+console.log(plant.species);
+
+plant.species = 'AB';
+console.log(plant.species);
+
+plant.species = 'Green';
+console.log(plant.species);
+
+// static properties and methods
+class Helper {
+    static PI: number = 3.14;
+    static calculateCircumference(diameter: number): number {
+        return this.PI * diameter;
+    }
+}
+console.log(Helper.PI);
+console.log(Helper.calculateCircumference(22));
+
+// abstract class
+abstract class Project {
+    projectName: string = 'default';
+    budget: number;
+
+    abstract changeName(name: string): void;
+
+    calcBudget(){
+        return this.budget * 2;
+    }
+}
+
+class ITProject extends Project {
+    changeName(name: string): void{
+        this.projectName = name
+    }
+}
+
+const project = new ITProject();
+project.changeName('kbms');
+
+// private constructors and singletons
+class OnlyOne {
+    private static instance: OnlyOne;
+
+    private constructor(public readonly name:string) {}
+
+    static getInstance(){
+        if (!OnlyOne.instance) {
+            OnlyOne.instance = new OnlyOne('The Only One');
+        }
+        return OnlyOne.instance;
+    }
+}
+let only = OnlyOne.getInstance();
