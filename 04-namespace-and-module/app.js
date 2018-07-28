0a8e2bf4f1a6ef0cd3d94499988e2a802b92f034
Archie Isdiningrat <archie.isdiningrat@bizzy.co.id>
2018-07-28 20:53:19 +0700

    finish namespace

--- 04-namespace-and-module/app.js
+++ 04-namespace-and-module/app.js
@@ -1,13 +1,3 @@
-var MyMath;
-(function (MyMath) {
-    var PI = 3.14;
-    function calculateCircumference(diameter) {
-        return diameter * PI;
-    }
-    MyMath.calculateCircumference = calculateCircumference;
-    function calculateRectangle(width, length) {
-        return width * length;
-    }
-    MyMath.calculateRectangle = calculateRectangle;
-})(MyMath || (MyMath = {}));
+/// <reference path="circleMath.ts" />
+/// <reference path="rectagleMath.ts" />
 console.log(MyMath.calculateRectangle(10, 20));
--- 04-namespace-and-module/app.ts
+++ 04-namespace-and-module/app.ts
@@ -1,13 +1,4 @@
-namespace MyMath {
-    const PI = 3.14;
-
-    export function calculateCircumference(diameter: number): number {
-        return diameter * PI;
-    }
-
-    export function calculateRectangle(width: number, length: number): number {
-        return width * length;
-    }
-}
+/// <reference path="circleMath.ts" />
+/// <reference path="rectagleMath.ts" />
 
 console.log(MyMath.calculateRectangle(10, 20));
