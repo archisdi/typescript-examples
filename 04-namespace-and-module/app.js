de6ac401b172c1e549d84113527df52611683d25
Archie Isdiningrat <archie.isdiningrat@bizzy.co.id>
2018-07-29 14:44:11 +0700

    finish namespace and module

--- 04-namespace-and-module/app.js
+++ 04-namespace-and-module/app.js
@@ -1,3 +1,15 @@
-/// <reference path="circleMath.ts" />
-/// <reference path="rectagleMath.ts" />
-console.log(MyMath.calculateRectangle(10, 20));
+"use strict";
+/**
+ * Namespaces
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+// / <reference path="circleMath.ts" />
+// / <reference path="rectagleMath.ts" />
+// console.log(MyMath.calculateRectangle(10, 20));
+/**
+ * Modules
+ */
+var circle_1 = require("./math/circle");
+var rectagle_1 = require("./math/rectagle");
+console.log(circle_1.calculateCircumference(20));
+console.log(rectagle_1.calculateRectangle(10, 20));
--- 04-namespace-and-module/app.ts
+++ 04-namespace-and-module/app.ts
@@ -1,4 +1,18 @@
-/// <reference path="circleMath.ts" />
-/// <reference path="rectagleMath.ts" />
+/**
+ * Namespaces
+ */
 
-console.log(MyMath.calculateRectangle(10, 20));
+// / <reference path="circleMath.ts" />
+// / <reference path="rectagleMath.ts" />
+
+// console.log(MyMath.calculateRectangle(10, 20));
+
+
+/**
+ * Modules
+ */
+
+ import * as Circle from './math/circle';
+ import calculateRectangle from './math/rectagle';
+ console.log(Circle.calculateCircumference(20))
+ console.log(calculateRectangle(10, 20));
