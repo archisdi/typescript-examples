b42aa45ef7a1603d57e7f6401046a91d57a3b895
Archie Isdiningrat <archie.isdiningrat@bizzy.co.id>
2018-08-01 15:47:12 +0700

    add third-party

---
+++ 08-third-party/app.js
@@ -0,0 +1,3 @@
+$("#app").css({ "backgroud-color": "green" });
+// typings install dt~jquery --global --save
+// npm install --save-dev @types/jquery
---
+++ 08-third-party/app.ts
@@ -0,0 +1,4 @@
+$("#app").css({"backgroud-color" : "green"})
+
+// typings install dt~jquery --global --save
+// npm install --save-dev @types/jquery
